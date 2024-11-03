e6702a46a6067b2fe7e9c9b304fe60474b959669
DhruvKs <dhruvk0014@gmail.com>
2024-11-03 10:53:19 +0530

    Added Testing Files

---
+++ BookingSystemProject.Tests/BookingServiceTests.cs
@@ -0,0 +1,15 @@
+using BookingSystemProject.Services;
+namespace BookingSystemProject.Tests
+{
+    public class BookingServiceTests
+    {
+        private BookingService _bookingService;
+        //private Mock<>
+        [Fact]
+        public void Test1()
+        {
+            
+
+        }
+    }
+}
